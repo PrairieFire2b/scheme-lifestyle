2aa15fc161037c0a23c3a246faa38988948bed6f
Prairie Fire <prairie-fire@outlook.com>
2025-07-17 00:30:52 +0800

    update contents directive

--- build.js
+++ build.js
@@ -21,19 +21,20 @@ const index = await fs.readdir('index')
 
 const links = index.map(value => `/index/${value.slice(0, -3)}.html`)
 
+let metadata = {}
+
 /**
  * @this {CompileContext}
  * @type {Handle}
- * @returns {false | undefined}
+ * @note Sequential execution: it must be evaluated after the for loop after
  */
 function contents(d) {
 
   this.tag('<ul')
   this.tag('>')
-  while(!links);
   for (const [i, link] of links.entries()) {
     this.tag('<li>');
-    this.tag(`<a href="${link}">${index[i]}</a>`);
+    this.tag(`<a href="${link}">${metadata[index[i]].title}</a>`);
     this.tag('</li>');
   }
   this.tag('</ul>')
@@ -41,12 +42,18 @@ function contents(d) {
 
 let extensions = { extensions: [gfm(), directive(), frontmatter()], htmlExtensions: [gfmHtml(), directiveHtml({contents}), frontmatterHtml()]}
 
-await fs.writeFile('dist/index.html', template_h5.replace('{body}', micromark(homepage, extensions), {
-    'flag': 'w+'
-}).replace('{title}', 'Scheme Lifestyle'))
-
 for (let content of index) {
     let html = micromark(await fs.readFile(`index/${content}`), extensions)
     // TODO: proper title (metadata)
-    await fs.writeFile(`dist/index/${content.slice(0, -3)}.html`, template_h5.replace('{body}', html).replace('{title}', content.slice(0, -3)))
+    let first_h1 = html.match(/\<h1\>([^<]*)\<\/h1\>/i)[0]
+    let title = first_h1 ? first_h1.slice(4, -5).trim() : content.slice(0, -3)
+    metadata[index] = { title };
+    await fs.writeFile(`dist/index/${content.slice(0, -3)}.html`, template_h5.replace('{body}', html).replace('{title}', title))
 }
+
+/**
+ * NOTE: @see {contents}
+ */
+await fs.writeFile('dist/index.html', template_h5.replace('{body}', micromark(homepage, extensions), {
+  'flag': 'w+'
+}).replace('{title}', 'Scheme Lifestyle'))
